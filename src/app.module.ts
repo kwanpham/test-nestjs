51c5dec43b9381b32baf44142354d49ca25704d2
Kwan Pham <chomchomcapital@gmail.com>
2023-08-18 00:02:18 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,22 @@ import { AuthModule } from './auth/auth.module';
 import { ServiceModule } from './service/service.module';
 import { AuthController } from './controller/auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '103.160.89.20',
+      port: 3306,
+      username: 'root',
+      password: 'lilama1996',
+      database: 'bank',
+      autoLoadEntities: true,
+      synchronize: false,
     }),
     AuthModule,
     ServiceModule,
